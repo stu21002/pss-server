91a4e625646b3a8fb63fb385e296ee03659e25d1
Artstu02 <stuartheath02@gmail.com>
2024-02-03 06:56:56 +0200

    Basic handling complete
    If multiple photos have the same name, it will add a counter to the end of the file.

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ app.post('/upload', upload.single('image'), async (req, res) => {
         
         //getting file data
         const {buffer,originalname} = req.file;
-        const metadata = await sharp(buffer).metadata();
+        const metadata = await sharp(buffer).metadata();   
+        const fileFormat = metadata.format;     
         const exifData = exifReader(metadata.exif);
 
         console.log(metadata)
@@ -39,10 +40,15 @@ app.post('/upload', upload.single('image'), async (req, res) => {
         const dateTaken = exifData ? exifData.Photo.DateTimeOriginal || exifData.Photo.DateTimeDigitized : null;
         const timestamp = dateTaken ? formatTimestamp(dateTaken) : Date.now();
         
-        const filePath = path.join(__dirname, 'uploads', originalname);
+        let filePath = path.join(__dirname, 'uploads', `${timestamp}.${fileFormat}`);
+        let dup = 1;
+        while (fs.existsSync(filePath)){
+            filePath = path.join(__dirname, 'uploads', `${timestamp}_${dup++}.${fileFormat}`);
+            
+        }
 
-        // Save the file asynchronously
-        require('fs').writeFile(filePath, buffer, (err) => {
+        // Save the file
+        fs.writeFile(filePath, buffer, (err) => {
         if (err) {
             console.error('Error saving file:', err);
             res.status(500).json({ message: 'Error saving file' });
